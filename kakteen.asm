932ab5c925bcaa60a39fbfd430e1851b03b73843
Heiko Schaefer <heiko.schaefer@swissonline.ch>
2015-05-02 20:29:09 +0200

    Working temperature output

--- kakteen.asm
+++ kakteen.asm
@@ -237,8 +237,8 @@ Div8by8X:           sec
 Div8by8Exit:        ret
 
 u2a:                ; convert unsigned in r17 to 3 bytes in buffer
-                    ldi XL, LOW(Buffer+3)
-                    ldi XH, HIGH(Buffer+3)
+                    ldi XL, LOW(LcdBuffer+3)
+                    ldi XH, HIGH(LcdBuffer+3)
                     ldi r18, 10
                     ldi r20, 3
 u2aNext:            rcall Div8by8
@@ -280,8 +280,8 @@ Timer0Delay:        push r18
                     out TCCR0B, r18
                     out TCNT0, r16   ; the counter to start with
                     out TCCR0B, r17  ; set prescaler also starts the clock
-Timer0DelayWait:    ;in r16, TIFR0
-                    ;andi r16, 1<<TOV0
+Timer0DelayWait:    ; checking against TOV0 bit in TIFR0 did not work
+                    ; now we use r18 as a flag, it gets set in the interrupt handler
                     tst r18
                     breq Timer0DelayWait
                     ; stop timer this is super important, also to set back TOV0
@@ -329,6 +329,24 @@ Timer0Delay15us:    push r16
                     pop r16
                     ret
 
+Timer0Delay100ms:   push r16
+                    push r17
+                    ldi r16, 38  ; -> count 217 times
+                    ldi r17, PRESCALE_1024
+                    rcall  Timer0Delay
+                    pop r17
+                    pop r16
+                    ret
+
+Timer0Delay1s:      push r18
+                    ldi r18, 10 
+Timer0Delay1sLoop:  rcall Timer0Delay100ms
+                    dec r18
+                    brne Timer0Delay1sLoop
+                    pop r18
+                    ret
+
+
 ; ----------------- ONE WIRE ------------------------------
 OneWireInit:        ; set bus to output and high
                     sbi ONEWIRE_DIR, ONEWIRE_BUS
@@ -345,10 +363,133 @@ OneWireReset:       ; reset bus and see if something is ther
                     cbi ONEWIRE_DIR, ONEWIRE_BUS ; to input
                     sbi ONEWIRE_PORT, ONEWIRE_BUS ; pull up to 1
                     rcall Timer0Delay44us  ; wait
+                    ; note that for reading we need to take the IN port!
                     sbis ONEWIRE_PORTIN, ONEWIRE_BUS  ; if it stays high, there is no one
                     ldi r16, 0xFF
                     rcall Timer0Delay500us  ; wait 500 us
                     ret
+
+OneWirePutBit:      ; put a single bit over the one wire interface, we use
+                    ; bit 0 of 16 -> one wire pushes least significant bit
+                    ; first if we transfer a byte. Thus we can just shift
+                    ; left the r16 value. We do not touch r16 otherwise
+                    sbi ONEWIRE_DIR, ONEWIRE_BUS ; set to output
+                    cbi ONEWIRE_PORT, ONEWIRE_BUS ; pull low
+                    rcall Delay_2us
+                    rcall Delay_2us
+
+                    ;rcall Timer0Delay15us ; minimum delay
+                    sbrs r16, 0 ;
+                    rjmp OneWirePut0
+                    ; we write a '1' so we pull up after the short 15us low
+                    cbi ONEWIRE_DIR, ONEWIRE_BUS ; make input
+                    sbi ONEWIRE_PORT, ONEWIRE_BUS ; pull up to 1
+OneWirePut0:        ; just keep port low for longer
+                    rcall Timer0Delay44us
+                    cbi ONEWIRE_DIR, ONEWIRE_BUS ; make input
+                    sbi ONEWIRE_PORT, ONEWIRE_BUS ; pull up to 1
+                    ; delay 2us
+                    rcall Delay_2us
+                    ret
+
+OneWireGetBit:     ; get a single bit over the one wire iterfacr
+                   ; place it in bit 7 of r16 without touching any
+                   ; of the other bits, so that we can shift it right
+                   ; one wire sends least significant bit first, so
+                   ; we can juse push right 8 times
+                   ; clear bit 7 in r16
+                   andi r16, 0x7F
+                   sbi ONEWIRE_DIR, ONEWIRE_BUS ; set to output
+                   cbi ONEWIRE_PORT, ONEWIRE_BUS; pull low
+                   rcall Delay_2us ; short wait
+                   cbi ONEWIRE_DIR, ONEWIRE_BUS ; set to input
+                   sbi ONEWIRE_PORT, ONEWIRE_BUS ; let it pull to one
+                   rcall Delay_2us   ; calibrated via osci....
+                   rcall Delay_2us
+                   rcall Delay_2us
+                   rcall Delay_2us
+                   rcall Delay_2us
+                   sbic ONEWIRE_PORTIN, ONEWIRE_BUS ; skip if bit is cleared
+                   ori r16, 0x80   ; set bit 7, as the bus is high
+                   rcall Timer0Delay44us
+                   ret
+
+OneWireWriteByte:  ; byte is in r16
+                   push r17   ; need a counter
+                   ldi r17, 8
+OWWriteLoop:       rcall OneWirePutBit
+                   lsr r16
+                   dec r17
+                   brne OWWriteLoop
+                   pop r17
+                   ret
+
+OneWireReadByte:   ; returns byte in r16
+                   push r17
+                   ldi r17, 8
+                   ldi r16, 0x00
+OWReadLoop:        lsr r16
+                   rcall OneWireGetBit
+                   dec r17
+                   brne OWReadLoop
+                   pop r17
+                   ret
+
+; ------------- DS18 B 20 stufff --------------
+
+DS1820Init:         ; --------- INIT DS18B20 : check it is there
+                    rcall OneWireReset
+                    ; found -> r16 = 0xff
+                    tst r16
+                    breq  DS1820NotFound
+                    ldi ZH, HIGH(2*DS1820OK)
+                    ldi ZL, LOW(2*DS1820OK)
+                    rcall LcdString
+                    ret
+DS1820NotFound:     ldi ZH, HIGH(2*Error)
+                    ldi ZL, LOW(2*Error)
+                    rcall LcdString
+                    rjmp End   ; finito
+
+
+
+
+DS1820ReadRam:    ; read all the ram and store it in DS1820Ram
+                  push ZL
+                  push ZH
+                  push r17
+                  push r16
+                  ldi ZH, HIGH(DS1820Ram)
+                  ldi ZL, LOW(DS1820Ram)
+
+                  rcall OneWireReset
+                  ldi r16, 0xCC  ; skip identification
+                  rcall OneWireWriteByte
+                  ldi r16, 0x44  ; start measuring temperature
+                  rcall OneWireWriteByte
+                  ; wait 1 second!
+                  rcall Timer0Delay1s
+                  
+                  rcall OneWireReset
+                  ldi r16, 0xCC  ; skip identification
+                  rcall OneWireWriteByte
+                  ldi r16, 0xBE  ; read ram
+                  rcall OneWireWriteByte
+                  
+                  ldi r17, 9;
+DS1820ReadLoop:   rcall OneWireReadByte
+                  st Z+, r16
+                  dec r17
+                  brne DS1820ReadLoop
+
+                  pop r16
+                  pop r17
+                  pop ZH
+                  pop ZL
+                  ret
+
+
+      
 
 
 
@@ -369,10 +510,6 @@ Reset:
                     rcall LcdInit
                     rcall LcdClearScreen
 
-                    ; debugging port
-                    sbi DDRB, 1  ;; for debugging
-                    cbi PORTB, 1
-
                     ; just some init message, not really needed
                     ; only for show
                     ldi ZH, HIGH(2*MsgLcd)
@@ -380,50 +517,113 @@ Reset:
                     rcall LcdString
                     rcall LongWait
                     rcall LcdClearScreen
+                    rcall LcdHome
 
-/*
-                    sbi PORTB, 1
-                    rcall Timer0Delay100us
-                    cbi PORTB, 1
-                    rcall Timer0Delay100us
-                    sbi PORTB, 1
-                    rcall Timer0Delay100us
-                    cbi PORTB, 1
-                    rcall Timer0Delay100us
-                    sbi PORTB, 1
-                    rcall Timer0Delay100us
-                    rjmp End
+                    rcall DS1820Init
+                    rcall Timer0Delay1s
 
-*/
+Again:                    rcall DS1820ReadRam
                     
-                    rcall OneWireReset
-                    ; found -> r16 = 0xff
-                    tst r16
-                    breq  DS1820NotFound
+                    rcall LcdClearScreen
+                    rcall LcdHome
 
-                    ldi ZH, HIGH(2*DS1820OK)
-                    ldi ZL, LOW(2*DS1820OK)
+                    ; *** somebody fucked around with r18, who?
+                    ldi r18, 0x00           ; use r0 to flag negative values
+                    mov r0, r18
+                    lds r16, DS1820Ram      ; lsb of temp
+                    lds r17, DS1820Ram+1    ; msb of temp
+                    ; handle negative temp
+                    sbrs r17, 7
+                    rjmp PositiveTemperature
+
+                    ; ok, bit 7 was set -> we have negative temperature
+                    ; take 2 complement and keep in mind that we had a negative number
+                    com r17
+                    neg r16  ; set carry
+                    adc r17, r18   ; r18 should be zero, so we just add the carry?
+                    ldi r18, 0xFF
+                    mov r0, r18
+
+PositiveTemperature:  ; at this point we have a positvie 16 bit number in r17:r16
+                    ; and r18 flags with 0xFF if we have < 0 temp
+                    push r16        ; we are going to ignore the lower nibble, i.e fractional part for noe
+                                    ; get rid of fractional part
+                    lsr r16
+                    lsr r16
+                    lsr r16
+                    lsr r16
+                    ; shift right, we do not need the most significant nibble
+                    lsl r17
+                    lsl r17
+                    lsl r17
+                    lsl r17
+                    add r17, r16  ; argument for u2a is in r17 -> inconsistent?
+                    rcall u2a  ; Now we have it in LcdBuffer
+                    ; this is where the result is
+                    ldi XL, LOW(LcdBuffer)
+                    ldi XH, HIGH(LcdBuffer)
+                    ;; we put a nice '+' in the front
+                    ldi r16, '+'
+                    st X, r16
+                    tst r0
+                    breq NoMinusSign
+                    ; overwrite the + with -
+                    ldi r16, '-'
+                    st X, r16
+NoMinusSign:        ld r16, X+
+                    rcall Lcd8BitData
+                    ld r16, X+
+                    rcall Lcd8BitData
+                    ld r16, X+
+                    rcall Lcd8BitData
+
+                    ; now the fractional part.
+                    pop r16
+                    andi r16, 0x0F    ;; wipe out higher nibble
+                    ; each fractional string is  8 bytes long so times 8
+                    lsl r16
+                    lsl r16
+                    lsl r16
+                    ldi ZL, LOW(2*Fractional)
+                    ldi ZH, HIGH(2*Fractional)
+                    add ZL, r16
+                    eor r17, r17
+                    adc ZH, r17
                     rcall LcdString
-                    rjmp End
-
-DS1820NotFound:     ldi ZH, HIGH(2*Error)
-                    ldi ZL, LOW(2*Error)
-                    rcall LcdString
-
-
+                    rjmp Again
                     
+
 End:
                     rjmp End
  
 
+;                      "0123456789012345"  ; maximal row length indicator
 MsgLcd:            .db "Starting....", 0, 0, 0
 DS1820OK:          .db "Found DS1820", 0, 0 
-Error:             .db "ERROR :-(", 0, 0 
+Error:             .db "DS1820 Dead?!", 0, 0 
+Fractional:        .db ".0000", 0, 0, 0  ; padding to 8 for easier addressing
+                   .db ".0625", 0, 0, 0
+                   .db ".1250", 0, 0, 0
+                   .db ".1875", 0, 0, 0
+                   .db ".2500", 0, 0, 0
+                   .db ".3125", 0, 0, 0
+                   .db ".3750", 0, 0, 0
+                   .db ".4375", 0, 0, 0
+                   .db ".5000", 0, 0, 0
+                   .db ".5625", 0, 0, 0
+                   .db ".6250", 0, 0, 0
+                   .db ".6875", 0, 0, 0
+                   .db ".7500", 0, 0, 0
+                   .db ".8125", 0, 0, 0
+                   .db ".8750", 0, 0, 0
+                   .db ".9375", 0, 0, 0
+                   .db 0, 0, 0
 
 
 .dseg ;------------  RAM  -------------------------------------
 
 .org SRAM_START
-Buffer: .byte 4
+DS1820Ram: .byte 10
+LcdBuffer: .byte 10  ; used for LCD output
 
 
